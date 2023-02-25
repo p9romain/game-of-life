0bd86394261656c2da463a93d63ecfe41994ee39
p9romain <romainperrod.pro9@gmail.com>
2023-02-25 22:43:07 +0100

    1

---
+++ gol.cpp
@@ -0,0 +1,92 @@
+#include <array>
+#include "SDL2/SDL.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+
+void drawPixels( SDL_Renderer* rd, const std::array<std::array<bool, WIDTH+2>, HEIGHT+2>* a, const int size = 20 )
+{
+  
+
+  for ( int i = 1 ; i < HEIGHT+1 ; i++ )
+  {
+    for ( int j = 1 ; j < WIDTH+1 ; j++ )
+    {
+      if ( (*a).at(i).at(j) )
+      {
+        SDL_SetRenderDrawColor(rd, 0, 0, 0, 255) ;
+      }
+      else
+      {
+        SDL_SetRenderDrawColor(rd, 255, 255, 255, 255) ;
+      }
+      SDL_Rect rect = { i*size, j*size, size, size } ;
+      SDL_RenderFillRect(rd, &rect) ;
+    }
+  }
+}
+
+int numberNeighbours( const std::array<std::array<bool, WIDTH+2>, HEIGHT+2>* a, const int i, const int j )
+{
+  return (*a).at(i-1).at(j-1) + (*a).at(i-1).at(j) + (*a).at(i-1).at(j+1) + (*a).at(i).at(j-1) + (*a).at(i).at(j+1) + (*a).at(i+1).at(j-1) + (*a).at(i+1).at(j) + (*a).at(i+1).at(j+1) ;
+}
+
+void update( std::array<std::array<bool, WIDTH+2>, HEIGHT+2>* a )
+{
+  for ( int i = 1 ; i < HEIGHT+1 ; i++ )
+  {
+    for ( int j = 1 ; j < WIDTH+1 ; j++ )
+    {
+      int n = numberNeighbours(a, i, j) ;
+      if ( (*a).at(i).at(j) )
+      {
+        if ( not ( n == 2 or n == 3 ) ) (*a)[i][j] = false ;
+      }
+      else
+      {
+        if ( n == 3 ) (*a)[i][j] = true ; 
+      }
+    }
+  }
+}
+
+int main(int argc, char const **argv)
+{
+  SDL_Init(SDL_INIT_VIDEO) ;
+
+  SDL_Window* wnd = SDL_CreateWindow("Game of Life", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE) ;
+  SDL_Renderer* rd = SDL_CreateRenderer(wnd, -1, 0) ;
+
+  SDL_SetRenderDrawColor(rd, 255, 255, 255, 255) ;
+  SDL_RenderClear(rd) ;
+
+  std::array<std::array<bool, WIDTH+2>, HEIGHT+2> a ;
+  for ( int i = 0 ; i < HEIGHT+2 ; i++ )
+  {
+    for ( int j = 0 ; j < WIDTH+2 ; j++ )
+    {
+      a[i][j] = false ;
+    }
+  }
+
+  bool quit = false ;
+  while ( not quit )
+  {
+    SDL_Event evt ;
+    while ( SDL_PollEvent(&evt) )
+    {
+      if ( evt.type == SDL_QUIT ) quit = true ;
+
+      update(&a) ;
+      drawPixels(rd, &a) ;
+    }
+
+    SDL_RenderPresent(rd) ;
+  }
+
+  SDL_DestroyRenderer(rd) ;
+  SDL_DestroyWindow(wnd) ;
+  SDL_Quit() ;
+ 
+  return 0 ;
+}
