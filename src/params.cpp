6120257ae0de8e1e239935405bd15fa58a3bc2c9
p9romain <romainperrod.pro9@gmail.com>
2023-11-06 19:38:43 +0100

    Fix mouse pos ?

--- src/params.cpp
+++ src/params.cpp
@@ -12,6 +12,6 @@ void Game::updateMousePos()
 {
   int x, y ;
   SDL_GetMouseState(&x, &y) ;
-  this->mouse_pos.x = int(this->origin.x - this->window.grid_height() / 2 + y / float(this->window.current_p_size)) ;
-  this->mouse_pos.y = int(this->origin.y - this->window.grid_width() / 2 + x / float(this->window.current_p_size)) ;
+  this->mouse_pos.x = int( this->origin.x - int ( this->window.grid_height() / 2 ) + y / float(this->window.current_p_size) ) ;
+  this->mouse_pos.y = int( this->origin.y - int ( this->window.grid_width() / 2 ) + x / float(this->window.current_p_size) ) ;
 }
